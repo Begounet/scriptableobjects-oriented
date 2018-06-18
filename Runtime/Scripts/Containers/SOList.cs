0a0f194456f494d3d507b2b28cc58b6be362fe6a
Arnaud Begue <abegue@persistant.fr>
2018-06-18 20:06:43 +0200

    + Prepare for lists

---
+++ Runtime/Scripts/Containers/SOList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SOList<ItemType> : SOBaseVariable
+{
+    public List<ItemType> list;
+
+    public void Add(ItemType item)
+    {
+        list.Add(item);
+    }
+}

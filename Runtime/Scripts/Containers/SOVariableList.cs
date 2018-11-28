2c86f6efd11ff1491d336cdb801fd0745845f7a8
Arnaud Begue <abegue@persistant.fr>
2018-11-28 16:49:03 +0100

    + Add containers to add generic SO variables + Add containers to add SO vector3

---
+++ Runtime/Scripts/Containers/SOVariableList.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SOVariableList", menuName = "SO/Containers/List/Generic SO Variables", order = 0)]
+public class SOVariableList : SOList<SOBaseVariable> {}
---
+++ Runtime/Scripts/Containers/Variables/Atomics/SOVector3List.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SOVector3List", menuName = "SO/Containers/List/Atomics/Vector3", order = 1)]
+public class SOVector3List : SOList<SOVector3> {}

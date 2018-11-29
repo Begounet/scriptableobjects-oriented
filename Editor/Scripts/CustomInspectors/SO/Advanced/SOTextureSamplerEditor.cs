bed8cb30b7375b9f7c11a88056ca44949216183f
Arnaud Begue <abegue@persistant.fr>
2018-11-29 11:28:57 +0100

    + Add a special SO variable that can be used to sample a texture easily from CPU

---
+++ Editor/Scripts/CustomInspectors/SO/Advanced/SOTextureSamplerEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SOTextureSampler))]
+public class SOTextureSamplerEditor : SOVariableEditor
+{
+    public override bool HasPreviewGUI()
+    {
+        SOTextureSampler textureSampler = target as SOTextureSampler;
+        return textureSampler.IsTextureValid;
+    }
+
+    public override void OnPreviewGUI(Rect r, GUIStyle background)
+    {
+        base.OnPreviewGUI(r, background);
+        SOTextureSampler textureSampler = target as SOTextureSampler;
+        GUI.DrawTexture(r, textureSampler.LookupTable);
+    }
+}
---
+++ Runtime/Scripts/Variables/Advanced/SOTextureSampler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SOTextureSampler", menuName = "SO/Advanced/Texture Sampler")]
+public class SOTextureSampler : SOBaseVariable
+{
+    [SerializeField]
+    private Texture2D _lookupTable;
+    public Texture2D LookupTable
+    {
+        get { return _lookupTable; }
+        set { _lookupTable = value; }
+    }
+
+    public bool IsTextureValid { get { return _lookupTable != null; } }
+    public int TextureWidth { get { return IsTextureValid ? _lookupTable.width : 0; } }
+    public int TextureHeight { get { return IsTextureValid ? _lookupTable.height : 0; } }
+
+    public Color SampleColor(int x, int y)
+    {
+        if (_lookupTable == null)
+        {
+            return Color.black;
+        }
+
+        x = Mathf.Clamp(x, 0, _lookupTable.width - 1);
+        y = Mathf.Clamp(y, 0, _lookupTable.height - 1);
+
+        return _lookupTable.GetPixel(x, y);
+    }
+}
